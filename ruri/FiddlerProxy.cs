557aefe8d91ccd287061458a956a7c5c3967b986
gooAya <37108352+gooaya@users.noreply.github.com>
2018-12-31 22:19:45 +1100

    v0.0.1

--- ruri/FiddlerProxy.cs
+++ ruri/FiddlerProxy.cs
@@ -54,6 +54,52 @@ namespace FiddlerCore.NetCore
         private void OnRequest(Session oS)
         {
             FiddlerApplication.Log.LogFormat("{0} {1}", oS.RequestMethod, oS.fullUrl);
+            if(oS.fullUrl== "http://ad2.nono.nyanbox.com:7080/static/server-list.json")
+            {
+                oS.utilCreateResponseAndBypassServer();
+                oS.oResponse.headers.SetStatus(200, "Ok");
+                oS.oResponse["Content-Type"] = "application/json; charset=UTF-8";
+                oS.utilSetResponseBody(@"[{""id"":""397"",""domain"":""androidprod.nono.nyanbox.com"",""name"":""test"",""utcOffset"":28800,""downloadLink"":""http://localhost/nono/nono_60112.apk"",""curVersion"":""0.7.8"",""minVersion"":""0.7.8"",""noticeUrl"":""http://notice.nyanbox.com:3300/"",""userTermsUrl"":""http://notice.nyanbox.com:3300/""}]");
+                return;
+            }
+            if (oS.fullUrl.StartsWith("http://as1.nono.nyanbox.com:8089/u8server/user/getToken"))
+            {
+                oS.utilCreateResponseAndBypassServer();
+                oS.oResponse.headers.SetStatus(200, "Ok");
+                oS.oResponse["Content-Type"] = "text/json; charset=UTF-8";
+                oS.utilSetResponseBody(@"{""data"":{""extension"":"""",""platID"":0,""sdkUserName"":""1000000000000"",""userID"":100000,""sdkUserID"":""100000000"",""username"":""1000000000000.isekai"",""token"":""ffffffffffffffffffffffffffffffff"",""timestamp"":""1546185625589""},""state"":1}");
+                return;
+            }
+            if (oS.fullUrl.StartsWith("http://trace2144.2144.cn/__beacon_sdk.gif"))
+            {
+                oS.utilCreateResponseAndBypassServer();
+                oS.oResponse.headers.SetStatus(200, "Ok");
+                oS.oResponse["Content-Type"] = "image/gif";
+                return;
+            }
+            if (oS.fullUrl.StartsWith("http://mapi.2144.cn/hook/start"))
+            {
+                oS.utilCreateResponseAndBypassServer();
+                oS.oResponse.headers.SetStatus(200, "Ok");
+                oS.oResponse["Content-Type"] = "application/json; charset=utf-8";
+                oS.utilSetResponseBody(@"{""ali_pay"":1,""weixin_pay"":1,""sdk_status"":0,""ball"":0,""welfare"":0,""game_id"":10012,""slug"":""nono"",""time"":1545898091,""mobile_find_password"":""0"",""email_find_password"":""0"",""contact_customer_servicer"":""800050781"",""customer_tel"":""4008-2144-26"",""customer_time"":""7*24小时"",""weixin_login"":0,""welfare_category_id"":4,""welfare_type_id"":1,""anti_addiction"":""1"",""ios_pay_third"":""0"",""user_center_default"":""3.0.0"",""yibao_pay"":1,""yibao_pay_card"":1,""guest_recharge"":0,""game_audit_version"":"""",""h5_weixin_pay"":1,""h5_ali_pay"":1,""h5_yibao_pay"":1,""h5_wechat_pay"":0,""i_p_url"":""https://tieba.baidu.com/f?kw=%E8%AF%BA%E8%AF%BA%E6%9D%A5%E8%87%AA%E5%BC%82%E4%B8%96%E7%95%8C&ie=utf-8"",""reyun_filter_slug"":""dhmy,dtxxz,tlz,ylws"",""real_auth"":1,""toutiao_android_app_id"":0,""social_data_version"":""sdk"",""status"":""success""}");
+                return;
+            }
+            if (oS.fullUrl == "http://cpa.mapi.2144.cn/v1/receive/info_login")
+            {
+                oS.utilCreateResponseAndBypassServer();
+                oS.oResponse.headers.SetStatus(200, "Ok");
+                oS.oResponse["Content-Type"] = "application/json; charset=utf-8";
+                oS.utilSetResponseBody(@"{""status"":""success""}");
+                return;
+            }
+            if (oS.fullUrl == "http://cpa.mapi.2144.cn/v1/receive/first_start")
+            {
+                oS.utilCreateResponseAndBypassServer();
+                oS.oResponse.headers.SetStatus(200, "Ok");
+                oS.oResponse["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8";
+                return;
+            }
             if (oS.host == "androidprod.nono.nyanbox.com:8082")
             {
                 try
