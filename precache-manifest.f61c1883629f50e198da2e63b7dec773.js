1b6bbb779d2b57e406bfe565d4fd5fd1f1150215
gooAya <37108352+gooaya@users.noreply.github.com>
2019-05-13 00:09:34 +1000

    build

---
+++ precache-manifest.f61c1883629f50e198da2e63b7dec773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7564a5348a79646acef3544104c30fe",
+    "url": "/IsekaiIkuNoRuri/index.html"
+  },
+  {
+    "revision": "688f0574ecf0a07901ce",
+    "url": "/IsekaiIkuNoRuri/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fe55856f3d32d777c177",
+    "url": "/IsekaiIkuNoRuri/static/js/2.243ec05d.chunk.js"
+  },
+  {
+    "revision": "688f0574ecf0a07901ce",
+    "url": "/IsekaiIkuNoRuri/static/js/main.b0a6326d.chunk.js"
+  },
+  {
+    "revision": "d27195e806e44640757e",
+    "url": "/IsekaiIkuNoRuri/static/js/runtime~main.11733eac.js"
+  }
+]);
